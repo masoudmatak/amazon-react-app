0fb89313d2a233a2f982b7bbdcc23ebf2fe06a6f
masoudmatak <41437679+masoudmatak@users.noreply.github.com>
2020-07-03 17:05:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-		<h1>Masoud </h1>
+		<h1>Masoud Magnus</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
